44aa845b104b24eda8f70caacda9c8183b09667a
Hudson Finn <hudson.finn@zohomail.com>
2023-09-04 10:28:03 -0400

    Working on site

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { Header } from './componenets/header';
+import { Main } from './componenets/main';
 import './css/app.css';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Main />
 		</>
 	);
 }
