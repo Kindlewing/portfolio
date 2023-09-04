d009cec77b946ffd77d13be915097fd1654e77ac
Hudson Finn <hudson.finn@zohomail.com>
2023-09-04 09:45:09 -0400

    Add project

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { Header } from './componenets/header';
+import './css/app.css';
+
+function App() {
+	return (
+		<>
+			<Header />
+		</>
+	);
+}
+
+export default App;
---
+++ src/componenets/header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+	return (
+		<header className="header">
+			<div className="header__logo__column">
+				<a className="header__logo" href="#">
+					Hudson Finn
+				</a>
+				<p className="header__subtitle">Web Developer</p>
+			</div>
+		</header>
+	);
+};
+
+export { Header };
