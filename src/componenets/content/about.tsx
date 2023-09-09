d22ad8b6434d0c1b8e2f68d91ae7ab38d07247cf
Hudson Finn <hudson.finn@zohomail.com>
2023-09-09 10:52:32 -0400

    Start on about section

--- src/componenets/content/about.tsx
+++ src/componenets/content/about.tsx
@@ -1,7 +1,12 @@
-
-
 const About = () => {
-    return (
-    
-    )
-}
+	return (
+		<section className="about">
+			<p className="aboutText">
+				I graduated in 2022 from Gordon College with a Bachelor of
+				Science degree in Computer Science.
+			</p>
+		</section>
+	);
+};
+
+export { About };
